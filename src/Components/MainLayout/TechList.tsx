e3376c4ab9809532cfba863defeba4102685c272
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-09-18 18:16:05 +0600

    Rresive Props and design Tech cart

--- src/Components/MainLayout/TechList.tsx
+++ src/Components/MainLayout/TechList.tsx
@@ -1,9 +1,31 @@
+import type { Technology } from "../../Types/TechType";
+import TechCard from "./TechCard";
 
+type Props = {
+  technologies: Technology[];
+  selected: Technology[];
+  handleAdd: (tech: Technology) => void;
+};
 
-const TechList = ({technologies}) => {
+const TechList = ({ technologies, selected, handleAdd }: Props) => {
   return (
-    <div>
-      
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+      {technologies.map((tech) => {
+        const isSelected = selected.some((item) => item.id === tech.id);
+        const isDisabled = selected.some(
+          (item) => item.id === tech.id
+        );
+
+        return (
+          <TechCard
+            key={tech.id}
+            tech={tech}
+            isSelected={isSelected}
+            isDisabled={isDisabled}
+            handleAdd={handleAdd}
+          />
+        );
+      })}
     </div>
   );
 };
